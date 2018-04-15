b62853ba3828f192660d7278f96803a8e9060a0c
vickykatoch <vickykatoch@yahoo.com>
2018-04-14 22:07:51 -0400

    changes

--- src/app/fxwidget/fxwidget.component.ts
+++ src/app/fxwidget/fxwidget.component.ts
@@ -1,19 +1,55 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ChangeDetectionStrategy, TemplateRef, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-fxwidget',
   templateUrl: './fxwidget.component.html',
-  styleUrls: ['./fxwidget.component.scss']
+  styleUrls: ['./fxwidget.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FxwidgetComponent implements OnInit {
-  width = 100;
+  width = 0;
+  data: PriceOrderQty[] = [];
 
-  @ViewChild('container') container : ElementRef;
+  @ViewChild('container') container: ElementRef;
+  @ViewChild('enlargedTemplate') enlargedTemplate: TemplateRef<any>;
 
-  constructor() { }
+  constructor(private changeDetecter: ChangeDetectorRef) {
+    this.data = DataBuilder.generateData(5);
 
-  ngOnInit() {
-    this.width = this.container.nativeElement.offsetHeight;
   }
 
+  ngOnInit() {
+  }
+  ngAfterViewInit() {
+    this.width = this.container.nativeElement.offsetHeight;
+    setTimeout(() => {
+      this.changeDetecter.markForCheck();
+    }, 0);
+  }
+}
+
+export interface PriceOrderQty {
+  bqty?: number;
+  baggQty?: number;
+  bb?: number;
+  ba?: number;
+  aqty?: number;
+  aaggQty?: number;
+
+}
+class DataBuilder {
+  static generateData(levels: number): PriceOrderQty[] {
+    const data: PriceOrderQty[] = [];
+    for (let ctr = 0; ctr < levels; ctr++) {
+      data.push({
+        ba: 101.30,
+        bb: 102.40,
+        bqty: 10,
+        baggQty: 20,
+        aqty: 35,
+        aaggQty: 70
+      });
+    }
+    return data;
+  }
 }
